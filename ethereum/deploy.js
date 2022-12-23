435ef1514f42c4a5125efea19d089dc15972395f
Artur Serra <artur_seilah@hotmail.com>
2022-12-22 21:55:02 -0300

    Added the initial code for the deploy script

---
+++ ethereum/deploy.js
@@ -0,0 +1,7 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+    
+)
