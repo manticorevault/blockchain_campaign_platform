b56e414f7abefe2c6817338590b61c065980c0c1
Artur Serra <artur_seilah@hotmail.com>
2023-01-07 12:02:08 -0300

    Refactored the index.tsx file, updated with the Semantic UI CSS and to the modern paradigm for NextJS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,37 @@
-import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 import factory from "../ethereum/factory";
 
-function CampaignIndex({ campaigns }: any) {
-    console.log("Campaigns", campaigns); 
-
-    return <h1> { campaigns[0] } </h1>
+interface Props {
+    campaigns: any[];
 }
 
-CampaignIndex.getInitialProps = async () => {
-    const campaigns = await factory.methods.getDeployedCampaigns().call()
+export default function Home({ campaigns }: Props) {
+    const items = campaigns.map((item: any) => {
+        return {
+            header: item,
+            description: <a> View Campaign </a>,
+            fluid: true,
+        };
+    });
 
-    return { campaigns };
-};
+    return (
+        <div>
+            <h3> Open Campaigns </h3>
+            <Card.Group items={items} />
+            <Button 
+                content="Create Campaign!"
+                icon="add circle"
+                labelPosition="right"
+                primary
+            />
+        </div>
+    );
+}
 
-export default CampaignIndex
+export async function getServerSideProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-
-
+    return { props: { campaigns } };
+}
