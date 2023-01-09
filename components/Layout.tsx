d61cf4dd04bd4fe010d3bd57c1706e10c6dc712b
Artur Serra <artur_seilah@hotmail.com>
2023-01-09 10:24:51 -0300

    Added a Header component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 const Layout = (props: any) => {
     return (
         <div> 
-            <h1>
-                HEADER
-            </h1>
+            <Header />
             {props.children}
             <h1>
                 FOOTER
