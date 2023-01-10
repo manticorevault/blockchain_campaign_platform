ad7bd27db67d5f34a1697f101ed7ca4c9cdb3512
Artur Serra <artur_seilah@hotmail.com>
2023-01-10 14:34:54 -0300

    Created the form style for the /campaigns/new route

--- pages/campaigns/new.tsx
+++ pages/campaigns/new.tsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { Form, Button } from "semantic-ui-react";
 
 const CampaignNew = class extends React.Component {
     render() {
-        return <h1> New Campaign! </h1>
+        return (
+        
+            <div>
+                <h3> Create a Campaign </h3>
+                
+                <Form>
+                    <Form.Field>
+                        <label>
+                            Campaign Details
+                        </label>
+
+                        <input 
+                            placeholder="Minimum Contribution"
+                        />
+                    </Form.Field>  
+
+                    <Button
+                        primary 
+                    >
+                        Create
+                    </Button>
+                </Form>
+            </div>
+
+        );
     }
 }
 
