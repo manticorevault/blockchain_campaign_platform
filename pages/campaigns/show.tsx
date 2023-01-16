6d760a131c6996e2fea3ea47ef0e6a2617575d58
Artur Serra <artur_seilah@hotmail.com>
2023-01-16 20:04:14 -0300

    Added a new show.tsx page to display details about a campaign, also fixed the supporting routes.ts to handle dynamic routing

---
+++ pages/campaigns/show.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class CampaignShow extends Component {
+    render() {
+        return <h3> Show </h3>
+    }
+}
+
+export default CampaignShow;
--- routes.ts
+++ routes.ts
@@ -1,4 +1,8 @@
 const routes = require('next-routes')();
 export {}
 
+routes
+    .add("/campaigns/new")
+    .add("/campaigns/:address", "/campaigns/show")
+
 module.exports = routes;
