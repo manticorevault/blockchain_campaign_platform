ddc0a563908b859f2f2a4e8acf423b7970003d78
Artur Serra <artur_seilah@hotmail.com>
2023-01-24 16:22:59 -0300

    Created the Create A Request page

--- pages/campaigns/requests/index.tsx
+++ pages/campaigns/requests/index.tsx
@@ -1,13 +1,44 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { NextPage } from "next";
+import { Button, Table } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import Campaign from "../../../ethereum/campaign";
+import { Link } from "../../../routes";
 
-class RequestIndex extends Component {
-    render() {
-        return (
+interface Props {
+    address: string;
+}
+
+const RequestIndex: NextPage<Props> = ({ address }) => {
+    const [campaign, setCampaign] = useState<Campaign | null>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        const loadCampaign = async () => {
+            const campaign = Campaign(address);
+            setCampaign(campaign);
+        };
+        loadCampaign();
+    }, [address]);
+
+    return (
+        <div>
             <h3>
                 Request List!
             </h3>
-        )
-    }
+            <Link href={`/campaigns/${address}/requests/new`}>
+                <a>
+                    <Button primary>Add Request</Button>    
+                </a>  
+            </Link>
+        </div>
+    )
+}
+
+RequestIndex.getInitialProps = async (ctx: NextPageContext) => {
+    const { address } = ctx.query;
+    return { address }
 }
 
 export default RequestIndex;
+``
