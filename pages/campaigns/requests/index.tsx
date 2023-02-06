7fc70faa9bfa1544c5a914107d271264b7496606
Artur Serra <artur_seilah@hotmail.com>
2023-02-06 15:12:34 -0300

    Request Page working

--- pages/campaigns/requests/index.tsx
+++ pages/campaigns/requests/index.tsx
@@ -1,44 +1,72 @@
-import React, { useState, useEffect } from "react";
-import { NextPage } from "next";
-import { Button, Table } from "semantic-ui-react";
-import { useRouter } from "next/router";
-import Campaign from "../../../ethereum/campaign";
-import { Link } from "../../../routes";
+import React, { Component } from 'react';
+import { Button, Table } from 'semantic-ui-react';
+import { Link } from '../../../routes';
+import Campaign from '../../../ethereum/campaign';
 
 interface Props {
-    address: string;
+  address: string;
+  requests: {
+    description: string;
+    value: string;
+    recipient: string;
+    approvalCount: number;
+    complete: boolean;
+  }[];
+  requestCount: number;
+  approversCount: number;
 }
 
-const RequestIndex: NextPage<Props> = ({ address }) => {
-    const [campaign, setCampaign] = useState<Campaign | null>(null);
-    const router = useRouter();
+class RequestIndex extends Component<Props> {
+  static async getInitialProps(props: any) {
+    const { address } = props.query;
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
 
-    useEffect(() => {
-        const loadCampaign = async () => {
-            const campaign = Campaign(address);
-            setCampaign(campaign);
-        };
-        loadCampaign();
-    }, [address]);
+    const requests = await Promise.all(
+      Array(parseInt(requestCount, 10))
+        .fill(null)
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+
+    return { address, requests, requestCount, approversCount };
+  }
+
+  render() {
+    const { Header, Row, HeaderCell, Body } = Table;
 
     return (
-        <div>
-            <h3>
-                Request List!
-            </h3>
-            <Link href={`/campaigns/${address}/requests/new`}>
-                <a>
-                    <Button primary>Add Request</Button>    
-                </a>  
-            </Link>
-        </div>
-    )
-}
+      <div>
+        <h3>
+          Requests
+        </h3>
 
-RequestIndex.getInitialProps = async (ctx: NextPageContext) => {
-    const { address } = ctx.query;
-    return { address }
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary floated="right" style={{ marginBottom: 10 }}>
+              Add Request
+            </Button>
+          </a>
+        </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+        </Table>
+        <div>Found {this.props.requestCount} requests.</div>
+      </div>
+    );
+  }
 }
 
 export default RequestIndex;
-``
