0daabdb57d2735d7f7bfd6b8a44c491bbf7ad704
Artur Serra <artur_seilah@hotmail.com>
2023-02-07 10:27:12 -0300

    Fixed the 404 on Campaign/Show

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -7,106 +7,90 @@ import ContributeForm from "../../components/ContributeForm";
 import { Link } from "../../routes";
 
 
-const CampaignShow = ({
-        address,
-        minimumContribution,
-        balance,
-        requestCount,
-        approversCount,
-        manager
-    }) => {
+class CampaignShow extends Component {
+    static async getInitialProps(props) {
+        const campaign = Campaign(props.query.address);
 
-        // Handle the type check for address
-        if (!address) return <p> Invalid address </p>
+        const summary = await campaign.methods.getSummary().call();
 
-        const router = useRouter();
-        const [campaign, setCampaign] = useState<Props | null>(null);
+        return {
+            address: props.query.address,
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+        };
+    }
 
-        useEffect(() => {
-            const getData = async () => {
-
-                // Type checking for address
-                if (typeof router.query.address !== "string") {
-                    return;
-                }
-
-                const campaign = Campaign(router.query.address);
-                const summary = await campaign.methods.getSummary().call();
-                
-                setCampaign({
-                    address: router.query.address,
-                    minimumContribution: summary[0],
-                    balance: summary[1],
-                    requestCount: summary[2],
-                    approversCount: summary[3],
-                    manager: summary[4],
-                });
-            };
-
-            getData();
-        }, [router.query.address]);
+    renderCards() {
+        const {
+            balance, 
+            manager,
+            minimumContribution,
+            requestsCount,
+            approversCount
+        } = this.props;
 
         const items = [
             {
                 header: manager,
-                meta: "Manager's address",
-                description: "Manager created this campaign",
-                style: { overflowWrap: "break-word" }
+                meta: 'Address of Manager',
+                description:
+                  'The manager created this campaign and can create requests to withdraw money',
+                style: { overflowWrap: 'break-word' }
             },
             {
                 header: minimumContribution,
-                meta: "Minimum Contribution in Wei",
-                description: "This should be the minimum contribution to become an approver",
+                meta: 'Minimum Contribution (wei)',
+                description:
+                  'You must contribute at least this much wei to become an approver'
             },
             {
-                header: requestCount,
-                meta: "Number of requests",
-                description: "A request will withdraw money from the campaign if successfully approved"
+                header: requestsCount,
+                meta: 'Number of Requests',
+                description:
+                  'A request tries to withdraw money from the contract. Requests must be approved by approvers'
             },
             {
                 header: approversCount,
-                meta: "Number of approvers",
-                description: "Number of people who have already donated to this campaign"
+                meta: 'Number of Approvers',
+                description:
+                  'Number of people who have already donated to this campaign'
             },
             {
-                header: web3.utils.fromWei(balance, "ether"),
-                meta: "Campaign balance in ETH",
-                description: "How much money this campaign has left to spend"
+                header: web3.utils.fromWei(balance, 'ether'),
+                meta: 'Campaign Balance (ether)',
+                description:
+                  'The balance is how much money this campaign has left to spend.'
             }
         ];
 
+        return <Card.Group items={ items } />;
+    }
+
+    render() {
         return (
             <div>
-                <h2>
-                    Campaign Address
-                </h2>
-
                 <h3>
-                    {campaign ? campaign.address : 'Loading...'}
+                    Campaign
                 </h3>
-
                 <Grid>
                     <Grid.Row>
                         <Grid.Column width={ 10 }>
-                            {campaign ? (
-                                <Card.Group items={ items } />
-                            ) : (
-                                <p>Loading...</p>
-                            )}
-                        </Grid.Column>
+                            { this.renderCards() }
+                        </Grid.Column> 
 
                         <Grid.Column width={ 6 }>
                             <ContributeForm 
-                                address={ router.query.address }
+                                address={ this.props.address }
                             />
                         </Grid.Column>
                     </Grid.Row>
 
                     <Grid.Row>
                         <Grid.Column>
-                            <Link 
-                                href={ `/campaigns/${ router.query.address }/requests` }
-                            >
+                            <Link route={ `/campaigns/${ this.props.address }/requests` }>
                                 <a>
                                     <Button
                                         primary
@@ -117,32 +101,10 @@ const CampaignShow = ({
                             </Link>
                         </Grid.Column>
                     </Grid.Row>
-
                 </Grid>
             </div>
         );
-};
-
-export const getServerSideProps = async () => {
-
-    // Type check for address
-    if(typeof ctx.query.address !== "string") {
-        return { props: {} };
     }
+}
 
-    const campaign = Campaign(ctx.query.address);
-    const summary = await campaign.methods.getSummary().call();
-
-    return {
-        props: {
-            address: ctx.query.address,
-            minimumContribution: summary[0],
-            balance: summary[1],
-            requestCount: summary[2],
-            approversCount: summary[3],
-            manager: summary[4],
-        },
-    };
-};
-
-export default CampaignShow;
+export default CampaignShow
