3b9502374cf0d205f474920b536e4836c7cc6e62
Artur Serra <artur_seilah@hotmail.com>
2023-02-07 09:34:02 -0300

    Updated for deploy

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+    metamask: {
+        TEST_SEED: "achieve draw wolf force oval tell rather denial slim nurse joke kick",
+    },
+    infura: {
+        GOERLI_TEST_NETWORK: "https://goerli.infura.io/v3/b21d16fea4e3416c9bb3262d18fca821",
+    },
+    factory: {
+        ADDRESS: "0x85D23B3988007443710C8dA4b0adA7Da0E3724d0",
+    }
+}
+
+module.exports = config;
